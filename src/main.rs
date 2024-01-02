1b3c8ce234f559e6b02f3ca9638a64f32d41633f
Grant Hulegaard <grant.hulegaard@yahooinc.com>
2024-01-01 21:17:05 -0800

    chore: some comments

--- src/main.rs
+++ src/main.rs
@@ -4,9 +4,11 @@ mod healthcheck;
 
 #[tokio::main]
 async fn main() {
+    // Create router/app
     let app = Router::new()
         .nest("/healthcheck", healthcheck::routes::get_router());
 
+    // Serve
     let listener = tokio::net::TcpListener::bind("localhost:4080").await.unwrap();
     axum::serve(listener, app).await.unwrap();
 }
